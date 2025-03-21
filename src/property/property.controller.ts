8b0a1602583dff6ae90f92cdf8a806f7e4fa4aec
gitbyul <gitbyul@gmail.com>
2025-03-21 11:47:32 +0900

    validationPipe (Decorator, Body Option)

--- src/property/property.controller.ts
+++ src/property/property.controller.ts
@@ -1,4 +1,16 @@
-import { Controller, Get, Param, ParseBoolPipe, ParseIntPipe, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseBoolPipe,
+  ParseIntPipe,
+  Post,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CreatePropertyDto } from './dto/createProperty.dto';
 
 @Controller('/property')
 export class PropertyController {
@@ -13,5 +25,22 @@ export class PropertyController {
     console.log({ id: sort, type: typeof sort });
 
     return id;
+  }
+
+  @Post('decorator')
+  @UsePipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
+  decoratorValidation(
+    @Body()
+    body: CreatePropertyDto,
+  ) {
+    return body;
+  }
+
+  @Post('body')
+  bodyValidation(
+    @Body(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
+    body: CreatePropertyDto,
+  ) {
+    return body;
   }
 }
