754071a0cd247e14dfd81d07858c8e4579570f66
gitbyul <gitbyul@gmail.com>
2025-03-25 17:28:22 +0900

    seed.ts env undefined 이슈로 인해 dotenv로 직접 호출

--- src/database/seeding/seed.ts
+++ src/database/seeding/seed.ts
@@ -1,13 +1,22 @@
+import * as dotenv from 'dotenv';
+import * as path from 'path';
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { SeederOptions, runSeeders } from 'typeorm-extension';
-import dbConfig from '../../config/db.config';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import { MainSeeder } from './main.seeder';
 import { PropertyFactory } from './property.factory';
 import { PropertyFeatureFactory } from './propertyFeature.factory';
 import { UserFactory } from './user.factory';
 
+dotenv.config();
+
 const options: DataSourceOptions & SeederOptions = {
-  ...dbConfig(),
+  url: process.env.DB_URL,
+  type: 'postgres',
+  port: Number(process.env.DB_PORT),
+  entities: [path.resolve(__dirname, '..') + '/**/*.entity{.ts,.js}'],
+  synchronize: true,
+  namingStrategy: new SnakeNamingStrategy(),
   factories: [PropertyFactory, UserFactory, PropertyFeatureFactory],
   seeds: [MainSeeder],
 };
